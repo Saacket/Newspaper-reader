200fb88d32e14bf27c2b8bf4259269e45ec421c1
Saacket <69721030+Saacket@users.noreply.github.com>
2021-07-29 10:48:30 +0530

    Update News Paper Reader .py

--- News Paper Reader .py
+++ News Paper Reader .py
@@ -9,4 +9,11 @@ def speak(str):
 if __name__ == '__main__':
     speak ("News for today.. Lets begin")
     url = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=205b68bcdfd94ab38d11a4440dc24b8f"
+    news = requests.get(url).text
+    news_dict = json.loads(news)
+    print(news_dict["articles"])
+    arts = news_dict['articles']
+    for article in arts:
+        speak(article['title'])
+        speak("Moving in to the next news .. Listen Carefully")
 
